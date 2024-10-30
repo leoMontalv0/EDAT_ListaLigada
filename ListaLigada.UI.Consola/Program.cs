61764dfc8a78bf4eee76153d0fbd0b9c2d83a543
leoMontalv0 <vigilleonel2004@gmail.com>
2024-10-30 07:25:43 -0600

    versión_02: Metodo Agregar y ToString.

--- ListaLigada.UI.Consola/Program.cs
+++ ListaLigada.UI.Consola/Program.cs
@@ -1,7 +1,17 @@
-﻿internal class Program
+﻿using ListaLigada.Logica;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hello, World! Lista Ligada simple \n");
+
+        var singleList = new SingleList<String>();
+
+        singleList.Add("Juan");
+        singleList.Add("José");
+        singleList.Add("Jan");
+
+        Console.WriteLine(singleList);
     }
 }
